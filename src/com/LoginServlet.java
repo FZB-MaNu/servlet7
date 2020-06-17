5b3aa8728bf872b95b2afa6389083e8865dd33c4
MaNu-Blog <1085533909@qq.com>
2020-06-17 15:48:43 +0800

    Initial commit

---
+++ src/com/LoginServlet.java
@@ -0,0 +1,27 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String account = request.getParameter("account");
+
+        HttpSession session=request.getSession();
+        if ("admin".equals(account)){
+            session.setAttribute("session_account",account);
+            session.setMaxInactiveInterval(30*60);
+            request.getRequestDispatcher("success").forward(request,response);
+        }else {
+            System.out.println("账号错误");
+            response.sendRedirect("/index.jsp");
+        }
+    }
+}
---
+++ src/com/SysFilter.java
@@ -0,0 +1,11 @@
+package com;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class SysFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        
+    }
+}
